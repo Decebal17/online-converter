8925e067dbdeaed7fac52bdc2d61ab7443cd6887
Decebal17 <eftenie.alin@gmail.com>
2025-10-20 02:15:48 +0200

    Use Vite entry; remove hashed /assets refs

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,5 @@ import App from './modules/App'
 import Help from './modules/Help'
 function Router(){const [h,setH]=React.useState(window.location.hash);React.useEffect(()=>{const f=()=>setH(window.location.hash);window.addEventListener('hashchange',f);return()=>window.removeEventListener('hashchange',f)},[]);const r=h.replace(/^#/,'');return r.startsWith('/help')? <Help/>:<App/>}
 createRoot(document.getElementById('root')!).render(<React.StrictMode><Router/></React.StrictMode>)
+
+import './styles.css';
